a204284e44f6fa0f2e30159fe03764c574f659fc
Azizbek Toksan-Akunov <20061168@stu.omu.edu.tr>
2021-11-23 22:07:16 +0300

    calc

---
+++ calculator.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+print 'Birici sayıyı giriniz: '
+num1 = gets.chomp.to_f
+print 'İkinci sayıyı giriniz: '
+num2 = gets.chomp.to_f
+print 'İşlemi seçiniz [+, -, *, /]: '
+action = gets.chomp
+
+if action == +
+  answer = num1 + num2
+  puts(answer)
+elsif action == -
+  answer = num1 - num2
+  puts(answer)
+elsif action == '*'
+  answer = num1 * num2
+  puts(answer)
+elsif action == '/'
+  answer = num1 / num2
+  puts(answer)
+else
+  puts 'Yanlış işlem seçtiniz!'
+end
