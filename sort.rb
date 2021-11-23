d67a70c9d5983f836b04460f5d81e3f11633835c
Azizbek Toksan-Akunov <20061168@stu.omu.edu.tr>
2021-11-23 19:37:32 +0300

    time

---
+++ sort.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+a = %w[c, a, b, x, z, y]
+
+b = a.sort
+
+puts "#{b}"
---
+++ test.rb
@@ -0,0 +1,9 @@
+a = 100
+
+while 
+  if a > 20
+    a -= 10
+  else
+    puts "#{a}"
+  end
+end
