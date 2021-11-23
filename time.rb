90e462e9f88c03b4f9541ccce213f99ea750a453
Azizbek Toksan-Akunov <20061168@stu.omu.edu.tr>
2021-11-23 21:11:30 +0300

    time

--- time.rb
+++ time.rb
@@ -5,27 +5,27 @@ hour = gets.chomp.to_i
 print 'Dakikayi giriniz: '
 minute = gets.chomp.to_i
 
-if (hour < 0 or hour > 23 )
+if hour < 0 || hour > 23
   print 'Geçerli saat girmediniz!'
-elsif (minute < 0 or minute > 59)
+elsif minute < 0 || minute > 59
   print 'Geçerli dakika girmediniz!'
-else 
-  if (minute == 0)
+else
+  if minute == 0
     print "Saat #{hour}"
   end
-  if (minute == 15)
+  if minute == 15
     print "Saat #{hour} çeyrek geçiyor"
   end
-  if (minute == 30)
+  if minute == 30
     print "Saat #{hour} buçuk"
   end
-  if (minute == 45)
+  if minute == 45
     print "Saat #{hour + 1} çeyrek var"
-  end 
-  if (minute > 0 and minute < 30)
-    print "Saat #{hour} : #{minute} geçiyor"
   end
-  if (minute >30 and minute < 60)
+  if minute > 0 && minute < 30
+    print "Saat #{hour}'den #{minute} geçiyor"
+  end
+  if minute > 30 && minute < 60
     print "Saat #{hour + 1}'e #{60 - minute} var"
   end
 end
